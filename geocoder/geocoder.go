9507683504ec5faee81c710372fe1dbf889a7a20
CiaraTully <ciara.tully@runa.io>
2023-11-05 19:09:22 +0000

    Make geocoder a package
    Add a folder called geocoder and add the geocoder.go file in there.
    
    Add an initialiser for the geocoderClient so that it can be created and
    used in other packages. Add functions to the geocoder which take the address
    as a string argument and retun an array of places to the caller.
    
    Example usage:
    ```
    import "whats-the-weather/main/geocoder"
    
    geoClient := geocoder.NewGeocoderClient()
    resp, err := geoClient.FindCoordinates("Statue of Liberty")
    ```
    	renamed:    geocoder.go -> geocoder/geocoder.go

---
+++ geocoder/geocoder.go
@@ -0,0 +1,41 @@
+package geocoder
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+const baseURL = "https://geocode.maps.co"
+const endpoint = "/search"
+
+type GeocoderClient struct{}
+
+type Place struct {
+	Latitude    string `json:"lat"`
+	Longitude   string `json:"lon"`
+	DisplayName string `json:"display_name"`
+}
+
+func NewGeocoderClient() *GeocoderClient {
+	return &GeocoderClient{}
+}
+
+func (g *GeocoderClient) FindCoordinates(address string) ([]Place, error) {
+
+	queryParams := url.Values{}
+	queryParams.Add("q", address)
+	fullURL := fmt.Sprintf("%s?%s", baseURL+endpoint, queryParams.Encode())
+	fmt.Println(fullURL)
+	resp, _ := http.Get(fullURL)
+
+	var places []Place
+
+	if err := json.NewDecoder(resp.Body).Decode(&places); err != nil {
+		fmt.Println("Error decoding JSON:", err)
+	}
+
+	defer resp.Body.Close()
+	return places, nil
+}
